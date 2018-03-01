d55d07993dd4b7fd1bd5a4c85636c292c66e7d13
hunter <864533822@qq.com>
2018-03-01 13:35:53 +0800

    test section fixed

--- Main.py
+++ Main.py
@@ -1,28 +1,33 @@
 import tensorflow as tf
 import numpy as np
 import os
-from PIL import Image,ImageOps
+import Image
+from PIL import ImageOps
 import glob
 import random
 import Image
 from os.path import join
 import net_struct
+import time
 
 ## sys_para
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 PATH = "/HDD/rdy/dataset/color_256" #250
 
-
 ## hyper_para
-BATCH_SIZE = 10 # size 256 RGB
+BATCH_SIZE = 20 # size 256 RGB
 LR = .0001
-BETA = .75
+BETA = .5
+epoch_step = 10000
 
 ## file input
 
 File_list = glob.glob(join(PATH,"*"))
 
-def get_img_batch(file_list,batch_size,img_size = (256,256)):
+# def normalize_batch(imgs):
+#     return(imgs - np.array([0.485, 0.456, 0.406])) / np.array([0.229, 0.224, 0.225])
+
+def get_img_batch(file_list,batch_size,img_size = (256,256),normalize = True):
     secret_batch = []
     cover_batch = []
 
@@ -33,7 +38,7 @@ def get_img_batch(file_list,batch_size,img_size = (256,256)):
         cover_img = Image.open(cover_path).convert("RGB")
 
         secret_img =  np.array(ImageOps.fit(secret_img,img_size),dtype = np.float32)/255
-        cover_img = np.array(ImageOps.fit(cover_img, img_size), dtype=np.float32) / 255
+        cover_img = np.array(ImageOps.fit(cover_img, img_size), dtype=np.float32)/255
 
         secret_batch.append(secret_img)
         cover_batch.append(cover_img)
@@ -41,13 +46,17 @@ def get_img_batch(file_list,batch_size,img_size = (256,256)):
     secret_batch = np.array(secret_batch)
     cover_batch = np.array(cover_batch)
 
+    # if normalize == True:
+    #     secret_batch = normalize_batch(secret_batch)
+    #     cover_batch = normalize_batch(cover_batch)
+
     return secret_batch,cover_batch
 
 def add_noise(tensor, std= .1):
     with tf.variable_scope("noise_layer"):
-        return tensor + tf.random_normal(shape = tf.shape(tensor), mean= 0.0, stddev=std, dtype=np.float32)
+        return tensor + tf.random_normal(shape=tf.shape(tensor), mean=0.0, stddev=std, dtype=np.float32)
 
-def get_loss(secret_true,secret_pred,cover_true,cover_pred,beta =.5):
+def get_loss(secret_true,secret_pred,cover_true,cover_pred,beta = BETA):
     ##modification in progress
     with tf.variable_scope("losses"):
         beta = tf.constant(beta, name="beta")
@@ -57,14 +66,15 @@ def get_loss(secret_true,secret_pred,cover_true,cover_pred,beta =.5):
         return final_loss, secret_mse, cover_mse
 
 def tensor2img(tensor):
-    with tf.variable_scope("",reuse=tf.AUTO_REUSE):
+    with tf.variable_scope("tensor2img",reuse=tf.AUTO_REUSE):
+        # t = tensor * tf.convert_to_tensor([0.229, 0.224, 0.225]) + tf.convert_to_tensor([0.485, 0.456, 0.406])
         return tf.clip_by_value(tensor,0,1)
 
 def training_graph(secret_tensor,cover_tensor,global_step):
-    with tf.variable_scope("training",reuse=tf.AUTO_REUSE):
+    with tf.variable_scope("",reuse=tf.AUTO_REUSE):
         prep_output = net_struct.prep_net(secret_tensor)
         hiding_output = net_struct.hiding_net(cover_tensor, prep_output)
-        noise_added_output =  add_noise(hiding_output)
+        noise_added_output = add_noise(hiding_output)
         rev_output = net_struct.rev_net(noise_added_output)
 
         loss,rev_loss,cover_loss = get_loss(secret_tensor,rev_output,cover_tensor, hiding_output)
@@ -83,8 +93,9 @@ def training_graph(secret_tensor,cover_tensor,global_step):
 
         return optimizer,merged_summary
 
+
 def test_graph(secret_tensor, cover_tensor):
-    with tf.variable_scope("testing",reuse=tf.AUTO_REUSE):
+    with tf.variable_scope("",reuse=tf.AUTO_REUSE):
         prep_output = net_struct.prep_net(secret_tensor)
         hiding_output = net_struct.hiding_net(cover_tensor, prep_output)
         rev_output = net_struct.rev_net(hiding_output)
@@ -104,28 +115,31 @@ def test_graph(secret_tensor, cover_tensor):
         return merged_summary
 
 
-
 with tf.Session() as sess:
-
-    writer = tf.summary.FileWriter("./logger")
+    global_step_tensor = tf.Variable(0, trainable=False, name="global_step")
 
     secret_tensor = tf.placeholder(shape=[None, 256, 256, 3],dtype=tf.float32,name="prep_input")
     cover_tensor = tf.placeholder(shape=[None, 256, 256, 3], dtype=tf.float32, name="hiding_input")
-    hiding_tensor = tf.placeholder(shape=[None, 256, 256, 3], dtype=tf.float32, name="deploy_covered")
+    writer = tf.summary.FileWriter("./logger")
 
-    global_step_tensor = tf.Variable(0, trainable=False, name="global_step")
 
     train_op, summary_op = training_graph(secret_tensor, cover_tensor, global_step_tensor)
-    test_op  = test_graph(secret_tensor,cover_tensor)
+    test_op = test_graph(secret_tensor,cover_tensor)
 
-    saver = tf.train.Saver(max_to_keep= 5 )
+    saver = tf.train.Saver(max_to_keep=5)
+    print('initializing...')
+    print('lr = %f'%LR)
+    print('beta = %f'%BETA)
+    print('batch size = %d'%BATCH_SIZE)
+    time.sleep(5)
 
-    epoch_step = 10000
     tf.global_variables_initializer().run()
+    writer.add_graph(sess.graph)
+
 
     for ep in range(10):
         for itr in range(epoch_step):
-            cover_img, secret_img = get_img_batch(file_list=File_list,batch_size = 32)
+            secret_img, cover_img = get_img_batch(file_list=File_list,batch_size = BATCH_SIZE)
 
             sess.run([train_op],feed_dict={secret_tensor:secret_img, cover_tensor:cover_img})
             os.system("clear")
@@ -133,14 +147,14 @@ with tf.Session() as sess:
 
 
             if itr % 10 == 0:
-                summary,global_step = sess.run([summary_op,global_step_tensor], feed_dict={secret_tensor:secret_img, cover_tensor:cover_img})
+                summary,global_step = sess.run([summary_op, global_step_tensor], feed_dict={secret_tensor:secret_img, cover_tensor:cover_img})
                 writer.add_summary(summary,global_step)
-                writer.add_graph(sess.graph)
+
 
             if itr % 100 == 0: #test section mulfunction need to debug
-                cover_img_test, secret_img_test = get_img_batch(file_list=File_list,batch_size=1)
-                summary, global_step = sess.run([test_op,global_step_tensor],feed_dict={secret_tensor:secret_img_test,cover_tensor:cover_img_test})
-                writer.add_summary(summary,global_step)
+                secret_img_test, cover_img_test = get_img_batch(file_list=File_list,batch_size=1)
+                summary, global_step = sess.run([test_op, global_step_tensor],feed_dict={secret_tensor:secret_img_test,cover_tensor:cover_img_test})
+                writer.add_summary(summary, global_step)
 
             save_model = saver.save(sess,"./model/"+"%d"%ep+".ckpt")
 
